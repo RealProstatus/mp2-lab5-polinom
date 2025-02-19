d32c04b4a5d37d367539f74bf0267c14be92e31b
prostatus <mel.sirvania@gmail.com>
2025-02-19 12:42:10 +0300

    Comparison operators for Monom added

--- mp2-lab5-polinom/Polinom.h
+++ mp2-lab5-polinom/Polinom.h
@@ -4,16 +4,16 @@ struct Monom {
 	double coeff;
 	int x, y, z;
 
-	bool operator>(const Monom& m) {
-
+	bool operator>(const Monom& m) const {
+		return (x * 100 + y * 10 + z) > (m.x * 100 + m.y * 10 + m.z);
 	}
-	bool operator>=(const Monom& m) {
-
+	bool operator>=(const Monom& m) const {
+		return (x * 100 + y * 10 + z) >= (m.x * 100 + m.y * 10 + m.z);
 	}
-	bool operator<(const Monom& m) {
-
+	bool operator<(const Monom& m) const {
+		return (x * 100 + y * 10 + z) < (m.x * 100 + m.y * 10 + m.z);
 	}
-	bool operator<=(const Monom& m) {
-
+	bool operator<=(const Monom& m) const {
+		return (x * 100 + y * 10 + z) <= (m.x * 100 + m.y * 10 + m.z);
 	}
 };
