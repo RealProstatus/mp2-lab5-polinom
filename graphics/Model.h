18b1aa29c36df6069a4d14c6e699aba6b7ba4b40
tmp <you@example.com>
2025-03-20 14:48:34 +0300

    Model header added

--- graphics/Model.h
+++ graphics/Model.h
@@ -8,5 +8,12 @@ class Model {
   std::vector<Polinom> polinomVector;
 
 public:
+  Model();
 
+  void addPolinomToVector(Polinom p);
+
+  void addPolinom(int indP1, int indP2);
+  void subPolinom(int indP1, int indP2);
+  void mulPolinom(int indP1, int indP2);
+  void mulPolinom(double cnst, int indP2);
 };
